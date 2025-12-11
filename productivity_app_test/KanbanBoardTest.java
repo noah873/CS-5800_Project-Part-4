288d29809bdaca2f9ed263bb87444de965409ff8
wtrinh02 <yelsew02@gmail.com>
2025-12-10 23:45:50 -0800

    Updated KanbanBoard and Task Unit Tests

--- productivity_app_test/KanbanBoardTest.java
+++ productivity_app_test/KanbanBoardTest.java
@@ -1,133 +1,55 @@
 package productivity_app_test;
 
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import productivity_app.KanbanBoard;
 import productivity_app.Task;
 
 public class KanbanBoardTest {
 
-    private KanbanBoard board;
+  private KanbanBoard board;
 
-    @Before
-    public void setUp() {
-        board = KanbanBoard.getInstance();
-        board.setTasks(new ArrayList<Task>());
-    }
+  @Before
+  public void setUp() {
+    board = KanbanBoard.getInstance();
+    board.setTasks(new java.util.ArrayList<Task>());
+  }
 
-    @Test
-    public void testGetInstance_BeforeInstanceInitialization() {
-        KanbanBoard b = KanbanBoard.getInstance();
-        assertNotNull(b);
-    }
+  @Test
+  public void testSingletonInstance() {
+    assertSame(board, KanbanBoard.getInstance());
+  }
 
-    @Test
-    public void testGetInstance_AfterInstanceInitialization() {
-        KanbanBoard a = KanbanBoard.getInstance();
-        KanbanBoard b = KanbanBoard.getInstance();
-        assertSame(a, b);
-    }
+  @Test
+  public void testAddAndListTasks() {
+    board.addTask(new Task("A", KanbanBoard.COLUMN_TODO));
+    board.addTask(new Task("B", KanbanBoard.COLUMN_DOING));
+    List<Task> tasks = board.getTasks();
+    assertEquals(2, tasks.size());
+  }
 
-    @Test
-    public void testSetTasks_WithValidList() {
-        ArrayList<Task> list = new ArrayList<>();
-        list.add(new Task("A", "To-Do"));
-        list.add(new Task("B", "Doing"));
-        board.setTasks(list);
-        assertEquals(2, board.getTasks().size());
-    }
+  @Test
+  public void testRemoveTaskByLabel() {
+    board.addTask(new Task("ToRemove", KanbanBoard.COLUMN_TODO));
+    board.removeTaskByLabel("ToRemove");
+    boolean exists = board.getTasks().stream()
+        .anyMatch(t -> t.getLabel().equals("ToRemove"));
+    assertFalse(exists);
+  }
 
-    @Test (expected = NullPointerException.class)
-    public void testSetTasks_WithNullList() {
-        board.setTasks(null);
-    }
-
-    @Test
-    public void testSetTasks_WithEmptyList() {
-        ArrayList<Task> empty = new ArrayList<>();
-        board.setTasks(empty);
-        assertTrue(board.getTasks().isEmpty());
-    }
-
-    @Test
-    public void testGetTasks_ReturnsExistingTasks() {
-        ArrayList<Task> list = new ArrayList<>();
-        list.add(new Task("A", "To-Do"));
-        board.setTasks(list);
-        assertEquals(1, board.getTasks().size());
-        assertEquals("A", board.getTasks().get(0).getLabel());
-    }
-
-    @Test
-    public void testGetTasks_ReturnsEmptyTaskList() {
-        board.setTasks(new ArrayList<Task>());
-        assertTrue(board.getTasks().isEmpty());
-    }
-
-    @Test
-    public void testAddTask_WithValidString() {
-        board.addTask("New Task");
-        assertEquals(1, board.getTasks().size());
-        assertEquals("New Task", board.getTasks().get(0).getLabel());
-        assertEquals("To-Do", board.getTasks().get(0).getStatus());
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testAddTask_WithNullString() {
-        board.addTask(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testAddTask_WithEmptyString() {
-        board.addTask("");
-    }
-
-    @Test
-    public void testRemoveTask_WithValidString() {
-        board.addTask("X");
-        board.removeTask("X");
-        assertEquals(0, board.getTasks().size());
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testRemoveTask_WithNullString() {
-        board.removeTask(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testRemoveTask_WithEmptyString() {
-        board.removeTask("");
-    }
-
-    @Test
-    public void testUpdateTask_WithValidInputs() {
-        board.addTask("T");
-        board.updateTask("T", "Doing");
-        assertEquals("Doing", board.getTasks().get(0).getStatus());
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testUpdateTask_WithANullInput() {
-        board.updateTask(null, "Doing");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testUpdateTask_WithAnEmptyInput() {
-        board.updateTask("", "Doing");
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testUpdateTask_WithNullNewColumn() {
-        board.addTask("T");
-        board.updateTask("T", null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testUpdateTask_WithEmptyNewColumn() {
-        board.addTask("T");
-        board.updateTask("T", "");
-    }
+  @Test
+  public void testUpdateTaskColumn() {
+    board.addTask(new Task("X", KanbanBoard.COLUMN_TODO));
+    board.updateTask("X", KanbanBoard.COLUMN_DONE);
+    Task updated = board.getTasks().stream()
+        .filter(t -> t.getLabel().equals("X"))
+        .findFirst()
+        .orElse(null);
+    assertNotNull(updated);
+    assertEquals(KanbanBoard.COLUMN_DONE, updated.getColumn());
+  }
 }
--- productivity_app_test/TaskTest.java
+++ productivity_app_test/TaskTest.java
@@ -6,92 +6,36 @@ import static org.junit.Assert.*;
 import productivity_app.Task;
 
 public class TaskTest {
-    @Test
-    public void testTaskConstructor_WithValidInputs() {
-        Task t = new Task("A", "To-Do");
-        assertNotNull(t);
-    }
 
-    @Test(expected = NullPointerException.class)
-    public void testTaskConstructor_WithNullLabel() {
-        new Task(null, "To-Do");
-    }
+  @Test
+  public void testTaskConstructorWithValidInputs() {
+    Task task = new Task("A", "To-Do");
+    assertNotNull(task);
+    assertEquals("A", task.getLabel());
+    assertEquals("To-Do", task.getColumn());
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testTaskConstructor_WithEmptyLabel() {
-        new Task("", "To-Do");
-    }
+  @Test(expected = NullPointerException.class)
+  public void testTaskConstructorWithNullLabel() {
+    new Task(null, "To-Do");
+  }
 
-    @Test(expected = NullPointerException.class)
-    public void testTaskConstructor_WithNullStatus() {
-        new Task("A", null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void testTaskConstructorWithEmptyLabel() {
+    new Task("   ", "To-Do");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testTaskConstructor_WithEmptyStatus() {
-        new Task("A", "");
-    }
+  @Test(expected = NullPointerException.class)
+  public void testTaskConstructorWithNullColumn() {
+    new Task("A", null);
+  }
 
-    @Test
-    public void testGetLabel_ReturnsCorrectLabel() {
-        Task t = new Task("A", "To-Do");
-        assertEquals("A", t.getLabel());
-    }
-
-    @Test
-    public void testSetLabel_WithValidLabel() {
-        Task t = new Task("A", "To-Do");
-        t.setLabel("B");
-        assertEquals("B", t.getLabel());
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testSetLabel_WithNullLabel() {
-        Task t = new Task("A", "To-Do");
-        t.setLabel(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetLabel_WithEmptyLabel() {
-        Task t = new Task("A", "To-Do");
-        t.setLabel("");
-    }
-
-    @Test
-    public void testGetStatus_AfterInitialization() {
-        Task t = new Task("A", "To-Do");
-        assertEquals("To-Do", t.getStatus());
-    }
-
-    @Test
-    public void testGetStatus_AfterUpdate() {
-        Task t = new Task("A", "To-Do");
-        t.setStatus("Doing");
-        assertEquals("Doing", t.getStatus());
-    }
-
-    @Test
-    public void testSetStatus_WithValidStatus() {
-        Task t = new Task("A", "To-Do");
-        t.setStatus("Done");
-        assertEquals("Done", t.getStatus());
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testSetStatus_WithNullStatus() {
-        Task t = new Task("A", "To-Do");
-        t.setStatus(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testSetStatus_WithEmptyStatus() {
-        Task t = new Task("A", "To-Do");
-        t.setStatus("");
-    }
-
-    @Test
-    public void testToString_Format() {
-        Task t = new Task("A", "Doing");
-        assertEquals("[Doing] A", t.toString());
-    }
+  @Test
+  public void testSettersUpdateValues() {
+    Task task = new Task("A", "To-Do");
+    task.setLabel("B");
+    task.setColumn("Doing");
+    assertEquals("B", task.getLabel());
+    assertEquals("Doing", task.getColumn());
+  }
 }
